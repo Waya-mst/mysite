dd1b533bef25bf95ecb51b67282c85dfac3add68
Waya-mst <111881126+Waya-mst@users.noreply.github.com>
2023-04-18 18:50:36 +0900

    ホームページto HP

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>わやのホームページ</title>
+        <title>わやのHP</title>
         <meta name="description" content="HP作成会用のサンプルページ" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
